cb64c23249611b7bc597588b3adb86a7d90bbc30
Lukas <majchrzyk88@gmail.com>
2024-07-28 00:56:22 +0200

    .

---
+++ !arch/1.Unique In Order.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+
+
+//UniqueInOrder("AAAABBBCCDAABBB");
+//
+//static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+//{
+//  List<T> list = new List<T>();
+//  bool bFirst = true;
+//  T previous = default(T);
+//  foreach (T current in iterable)
+//  {
+//    if (!bFirst)
+//    {
+//      if (!previous.Equals(current)) list.Add(current);
+//    }
+//    else
+//    {
+//      bFirst = false;
+//      list.Add(current);
+//    }
+//    previous = current;
+//  }
+//  return list;
+//}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+
+perimeter(5242);
+
+static BigInteger perimeter(BigInteger n)
+{
+  BigInteger res =0 ;
+  List<BigInteger> listFibonacci = new List<BigInteger> { 1, 1, 2 };
+  if (n >= 3)
+  {
+    for (int i = 1; i < n - 1; i++)
+    {
+      listFibonacci.Add(listFibonacci.ElementAt(i) + listFibonacci.ElementAt(i + 1));
+    }
+  }
+  foreach (BigInteger bi in listFibonacci)
+  {
+      res += bi;
+  }
+  return 4*res;
+}
+
+
